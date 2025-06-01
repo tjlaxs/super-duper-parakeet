1c7684063ac3284669a0ceb009544b75e8e787a3
Tero Laxström <tlax@neonpeons.com>
2025-06-01 16:09:00 +0300

    Movement for parakeet

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use bevy::prelude::*;
+use bevy::{core_pipeline::prepass::MotionVectorPrepass, prelude::*};
 
 const PARAKEET_SIZE: Vec2 = Vec2::new(60.0, 60.0);
+const PARAKEET_SPEED: f32 = 400.0;
 
 #[derive(Component)]
 struct Collider;
@@ -22,9 +23,28 @@ fn setup(mut commands: Commands) {
     ));
 }
 
+fn move_parakeet(
+    input: Res<ButtonInput<KeyCode>>,
+    mut parakeet: Single<&mut Transform, With<Parakeet>>,
+    time: Res<Time>,
+) {
+    let mut direction = 0.0;
+
+    if input.pressed(KeyCode::KeyH) {
+        direction -= 1.0;
+    }
+
+    if input.pressed(KeyCode::KeyL) {
+        direction += 1.0;
+    }
+
+    parakeet.translation.x += direction * PARAKEET_SPEED * time.delta_secs();
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(Update, move_parakeet)
         .run();
 }
