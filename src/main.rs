5374dcdd8707c3166772d06e3bb0f4ee16a162b6
Tero Laxström <tlax@neonpeons.com>
2025-06-01 15:48:47 +0300

    Just a parakeet

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
 use bevy::prelude::*;
 
-fn setup() {
-    println!("setup")
+const PARAKEET_SIZE: Vec2 = Vec2::new(60.0, 60.0);
+
+#[derive(Component)]
+struct Collider;
+
+#[derive(Component)]
+struct Parakeet;
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
+    commands.spawn((
+        Parakeet,
+        Sprite::from_color(Color::WHITE, Vec2::ONE),
+        Transform {
+            translation: Vec3::new(0.0, -300.0, 0.0),
+            scale: PARAKEET_SIZE.extend(1.0),
+            ..default()
+        },
+        Collider,
+    ));
 }
 
 fn main() {
